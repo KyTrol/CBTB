fd1242debfef0824d04d2870176bd835ac682847
KyTrol <ktrout@ah-ktrout.assurerx.local>
2015-11-17 13:07:50 -0500

    css tweaks and js for social media captions

--- js/script.js
+++ js/script.js
@@ -1,35 +1,23 @@
-/*
-* Replace all SVG images with inline SVG
-*/
-/*
 $(document).ready(function() {
-    $('img.svg').each(function(){
-        var $img = $(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-        console.log(imgURL);
-        $.get(imgURL, function(data) {
-            // Get the SVG tag, ignore the rest
-            var $svg = $(data).find('svg');
+    var fb = $("#fb");
+    var tw = $("#tw");
+    var inst = $("#inst");
+    var fbName = $("#fb-name");
+    var twName = $("#tw-name");
+    var instName = $("#inst-name");
 
-            // Add replaced image's ID to the new SVG
-            if(typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-            // Add replaced image's classes to the new SVG
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
-            }
+    fb.hover(
+        function () { fbName.stop().fadeIn(250); },
+        function () { fbName.stop().fadeOut(250); }
+    );
 
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg = $svg.removeAttr('xmlns:a');
+    tw.hover(
+        function () { twName.stop().fadeIn(250); },
+        function () { twName.stop().fadeOut(250); }
+    );
 
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-
-        }, 'xml');
-
-    });
+    inst.hover(
+        function () { instName.stop().fadeIn(250); },
+        function () { instName.stop().fadeOut(250); }
+    );
 });
-*/
