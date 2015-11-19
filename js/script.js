7ebd2256f81698bdfc2eae0d0d408b99ea91a3b7
KyTrol <ktrout@ah-ktrout.assurerx.local>
2015-11-19 11:03:50 -0500

    removed flexbox dependency

--- js/script.js
+++ js/script.js
@@ -7,17 +7,17 @@ $(document).ready(function() {
     var instName = $("#inst-name");
 
     fb.hover(
-        function () { fbName.stop().fadeIn(250); },
-        function () { fbName.stop().fadeOut(250); }
+        function () { fbName.stop().fadeTo(250, 1); },
+        function () { fbName.stop().fadeTo(250, 0); }
     );
 
     tw.hover(
-        function () { twName.stop().fadeIn(250); },
-        function () { twName.stop().fadeOut(250); }
+        function () { twName.stop().fadeTo(250, 1); },
+        function () { twName.stop().fadeTo(250, 0); }
     );
 
     inst.hover(
-        function () { instName.stop().fadeIn(250); },
-        function () { instName.stop().fadeOut(250); }
+        function () { instName.stop().fadeTo(250, 1); },
+        function () { instName.stop().fadeTo(250, 0); }
     );
 });
